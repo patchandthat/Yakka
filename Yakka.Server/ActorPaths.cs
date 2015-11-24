d15b875dede99cb04b0d235c6f0d3de3f761ae9f
Patch <patchandthat@gmail.com>
2015-11-24 13:48:21 +0000

    Revert "Revert "Redesign/regorganise for proper client""
    This reverts commit 0f078974d11d2afd96f77329a65f57c585f1715d.

---
+++ Yakka.Server/ActorPaths.cs
@@ -0,0 +1,30 @@
+﻿namespace Yakka.Server
+{
+    static class ActorPaths
+    {
+        public static readonly ActorMetaData ExampleActor = new ActorMetaData("ActorName");
+        public static readonly ActorMetaData ExampleChild = new ActorMetaData("ChildName", ExampleActor);
+    }
+    
+    /// <summary>
+    /// Meta-data class. Nested/child actors can build path
+    /// based on their parent(s) / position in hierarchy.
+    /// </summary>
+    public class ActorMetaData
+    {
+        public ActorMetaData(string name, ActorMetaData parent = null)
+        {
+            Name = name;
+            Parent = parent;
+            // if no parent, we assume a top-level actor
+            var parentPath = parent != null ? parent.Path : "/user";
+            Path = string.Format("{0}/{1}", parentPath, Name);
+        }
+
+        public string Name { get; private set; }
+
+        public ActorMetaData Parent { get; set; }
+
+        public string Path { get; private set; }
+    }
+}
