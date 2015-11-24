0f078974d11d2afd96f77329a65f57c585f1715d
Patch <patchandthat@gmail.com>
2015-11-24 13:47:16 +0000

    Revert "Redesign/regorganise for proper client"
    This reverts commit 71070a9e0bd33679cb63c71a1aa4f234a98a7388.

--- Yakka.Server/Actors/ConversationCoordinatorActor.cs
+++ Yakka.Server/Actors/ConversationCoordinatorActor.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace Yakka.Server.Actors
 {
-    class ConversationCoordinatorActor
+    //Probably need both server and client versions of these actors
+
+    class ConversationCoordinatorActor : ReceiveActor
     {
+        //Todo: Root level actor responsible for instantiating conversations and delivering messages to the appropriate conversations
+    }
+
+    class ConversationActor : ReceiveActor
+    {
+        //Todo: A conversation between 2 or more connected users
+
+        //Messages will be passed in from the coordinator, which will be delivered to all participants of the conversation
+
+        //Should handle people joining and leaving the conversation
+        //Should probably also have a Guid identifier
     }
 }
--- Yakka.Server/Actors/MessageRouterActor.cs
+++ Yakka.Server/Actors/MessageRouterActor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Yakka.Server.Actors
 {
-    class ClientCoordinatorActor
+    class MessageRouterActor
     {
     }
 }
