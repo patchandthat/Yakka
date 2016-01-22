65246d1c9eebf34bcb4590308ceff5959e8b3ff0
Patch <patchandthat@gmail.com>
2016-01-22 17:32:09 +0000

    Akka tests project + stub

---
+++ Yakka.Tests/SettingsActorTests.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using Xunit;
+using Akka.TestKit.Xunit2;
+using Yakka.Actors;
+
+namespace Yakka.Tests
+{
+    public class SettingsActorTests : TestKit
+    {
+        [Fact]
+        public void Demo()
+        {
+            var actor = Sys.ActorOf(Props.Create(() => new SettingsActor()));
+            actor.Tell("message");
+
+            bool result = ExpectMsg<bool>();
+
+            Assert.True(result);
+        }
+    }
+}
